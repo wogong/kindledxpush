52306859616973bcb6a14b436551506381979f71
lord63 <lord63.j@gmail.com>
2014-04-18 22:45:25 +0800

    edit variable name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding=UTF-8 -*-
 
-from os import path
-from datetime import datetime
 import logging
 import sqlite3
 import sys
+from os import path
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,11 +16,11 @@ if hasattr(sys, 'setdefaultencoding'):
     sys.setdefaultencoding('UTF-8')
 
 
-db = sqlite3.connect(sys.path[0], 'main.db')
+db = sqlite3.connect(path.join(sys.path[0], 'main.db'))
 cursor = db.cursor()
 
-session = requests.session()
-session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+s = requests.session()
+s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
 
 def get_hidden_form_data(form):
     ret = dict()
@@ -30,26 +30,26 @@ def get_hidden_form_data(form):
 
 def login(email, password):
     logging.info('Login...')
-    LOGIN_URL = 'https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin'
-    LOGIN_POST_URL = 'https://www.amazon.com/ap/signin'
-    login_page = BeautifulSoup(session.get(LOGIN_URL).text)
+    login_url = 'https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin'
+    login_post_url = 'https://www.amazon.com/ap/signin'
+    login_page = BeautifulSoup(s.get(login_url).text)
     form = login_page.find('form', id='ap_signin_form')
     data = get_hidden_form_data(form)
     data.update({'email': email, 'password': password})
-    session.post(LOGIN_POST_URL, data)
+    s.post(login_post_url, data)
 
 def get_contents():
     URL = 'https://www.amazon.com/gp/digital/fiona/manage/features/order-history/ajax/queryPdocs.html'
-    req = session.post(URL, {'offset': 0, 'count': 15, 
+    r = s.post(URL, {'offset': 0, 'count': 15, 
         'contentType': 'Personal Document', 'queryToken': 0, 'isAjax': 1})
     return [{'category': i['category'], 'contentName': i['asin']} 
-            for i in req.json()['data']['items']]
+            for i in r.json()['data']['items']]
 
 def deliver_content(content):
     URL = 'https://www.amazon.com/gp/digital/fiona/content-download/fiona-ajax.html/ref=kinw_myk_ro_send'
     content.update({'isAjax': '1', 'deviceID': DEVICE})
-    req = session.post(URL, content)
-    assert req.json()['data'] == 1
+    r = s.post(URL, content)
+    assert r.json()['data'] == 1
 
 def deliver_all(contents):
     logging.info('Delivering...')
