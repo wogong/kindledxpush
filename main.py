ea9a9af5d7644826dfcda31d902f3f3be522e3bd
lord63 <lord63.j@gmail.com>
2014-04-24 19:43:16 +0800

    code style enhance

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import sqlite3
 import sys
 import re
 from os import path
-from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -21,7 +20,9 @@ db = sqlite3.connect(path.join(sys.path[0], 'main.db'))
 cursor = db.cursor()
 
 s = requests.session()
-s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
+                   rv:28.0) Gecko/20100101 Firefox/28.0'})
+
 
 def translate(raw_title):
     def trans(unicode):
@@ -34,8 +35,9 @@ def translate(raw_title):
 
     title = ''
     for character in characters:
-        title = title + trans(character) 
+        title = title + trans(character)
     return title + date
+
 
 def get_hidden_form_data(page):
     hidden_form_data = dict()
@@ -44,30 +46,44 @@ def get_hidden_form_data(page):
         hidden_form_data[tag['name']] = tag['value']
     return hidden_form_data
 
+
 def login(email, password):
     logging.info('Login...')
-    login_url = 'https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin'
+    login_url = ('https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc'
+        '_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth'
+        '%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.'
+        'net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid'
+        '.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A'
+        '%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth'
+        '_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore'
+        '%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin')
     login_post_url = 'https://www.amazon.com/ap/signin'
     login_page = BeautifulSoup(s.get(login_url).text)
     data = get_hidden_form_data(login_page)
     data.update({'email': email, 'password': password})
     s.post(login_post_url, data)
 
+
 def get_contents():
-    url = 'https://www.amazon.com/gp/digital/fiona/manage/features/order-history/ajax/queryPdocs.html'
-    r = s.post(url, {'offset': 0, 'count': COUNT, 
+    url = ('https://www.amazon.com/gp/digital/fiona/manage/features/'
+           'order-history/ajax/queryPdocs.html')
+    r = s.post(url, {'offset': 0, 'count': COUNT,
         'contentType': 'Personal Document', 'queryToken': 0, 'isAjax': 1})
-    return [{'category': item['category'], 'contentName': item['asin'], 'title': item['title']} 
-            for item in r.json()['data']['items']]
+    return [{'category': item['category'], 'contentName': item['asin'],
+            'title': item['title']} for item in r.json()['data']['items']]
+
 
 def deliver_content(content):
-    url = 'https://www.amazon.com/gp/digital/fiona/content-download/fiona-ajax.html/ref=kinw_myk_ro_send'
+    url = ('https://www.amazon.com/gp/digital/fiona/content-download/'
+           'fiona-ajax.html/ref=kinw_myk_ro_send')
     content.update({'isAjax': '1', 'deviceID': DEVICE})
     r = s.post(url, content)
     assert r.json()['data'] == 1
 
+
 def deliver_all(contents):
     logging.info('Delivering...')
+
     def contentInDB(content):
         try:
             cursor.execute('select * from content where name = "%s"' % content)
@@ -87,13 +103,14 @@ def deliver_all(contents):
             pass
         else:
             logging.info('Done. Save to db.')
-            cursor.execute('insert into content values ("%s")' % content['contentName'])
-
+            cursor.execute('insert into content values ("%s")' %
+                           content['contentName'])
     db.commit()
 
+
 if __name__ == '__main__':
-    logging.basicConfig(filename=path.join(sys.path[0], 'main.log'), 
-                        level='INFO', 
+    logging.basicConfig(filename=path.join(sys.path[0], 'main.log'),
+                        level='INFO',
                         format='%(asctime)s [%(levelname)s] %(message)s')
     login(EMAIL, PASSWORD)
     deliver_all(get_contents())
