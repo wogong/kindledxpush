258c977b85a7b744afe0462f950813e8a6f9f099
lord63 <lord63.j@gmail.com>
2014-04-24 21:21:20 +0800

    add comment and docstring

--- main.py
+++ main.py
@@ -11,24 +11,29 @@ import requests
 from bs4 import BeautifulSoup
 from config import EMAIL, PASSWORD, DEVICE, COUNT
 
-
+# Set default encoding from ascii to utf-8
 reload(sys)
 if hasattr(sys, 'setdefaultencoding'):
     sys.setdefaultencoding('utf-8')
 
+# Connect to the database which lies in the current directory
 db = sqlite3.connect(path.join(sys.path[0], 'main.db'))
 cursor = db.cursor()
 
+# User-agent is necessary, or it'll occur `KeyError: 'data' `.
 s = requests.session()
 s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
                    rv:28.0) Gecko/20100101 Firefox/28.0'})
 
 
 def translate(raw_title):
+    '''translate escape characters to utf-8
+
+    To support chinese in log.'''
     def trans(unicode):
         return unichr(int(unicode, 16)).encode('utf-8')
 
-    if not raw_title.find(';'):
+    if not '&#x' in raw_title:
         return raw_title
     characters = re.findall(r'(?<=&#x).{4}', raw_title)
     date = raw_title.split(';')[-1]
@@ -36,10 +41,13 @@ def translate(raw_title):
     title = ''
     for character in characters:
         title = title + trans(character)
-    return title + date
+    return title + date   # Date is a configuration in my KindleEar
 
 
 def get_hidden_form_data(page):
+    '''Get hidden form data from the page after you click the signin button
+
+    Those data are needed to post to sign in.'''
     hidden_form_data = dict()
     input_tags = page.findAll('input', type='hidden')
     for tag in input_tags:
@@ -48,6 +56,11 @@ def get_hidden_form_data(page):
 
 
 def login(email, password):
+    '''To login in to amazom.com
+
+    The long url is the 'sign in' botton, the short is the 'sign in our 
+    secure sever' botton.'''
+
     logging.info('Login...')
     login_url = ('https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc'
         '_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth'
@@ -78,10 +91,13 @@ def deliver_content(content):
            'fiona-ajax.html/ref=kinw_myk_ro_send')
     content.update({'isAjax': '1', 'deviceID': DEVICE})
     r = s.post(url, content)
-    assert r.json()['data'] == 1
+    assert r.json()['data'] == 1    # Whether successfully delivered it
 
 
 def deliver_all(contents):
+    '''Deliver all the contents which haven't been delivered before
+
+    Once delivered it, save it's asin in database.'''
     logging.info('Delivering...')
 
     def contentInDB(content):
