e42f58eae148d3bce6f7e76eac1abd29a99cae44
lord63 <lord63.j@gmail.com>
2014-09-24 20:48:56 +0800

    Refine package
    - include readme, changelog, doc in package
    - rename CHANGLOG to ChangeLog

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ from setuptools import setup
 
 from kindlepush import kindlepush
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    with open('README.md') as f:
+        long_description = f.read()
+
 setup(
     name='kindlepush',
     version=kindlepush.__version__,
     description='Automatically send your doc to your kindle without clicking the deliver button for 3G device',
+    long_description=long_description,
     url='https://github.com/lord63/kindledxpush',
     author='lord63',
     author_email='lord63.j@gmail.com',
@@ -25,6 +33,7 @@ setup(
     keywords='kindle push automatically 3G',
     packages=['kindlepush'],
     install_requires=['requests', 'beautifulsoup4', 'terminal'],
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'kindlepush=kindlepush.kindlepush:main',
